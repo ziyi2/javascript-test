ab176e1b798e4f84ecb53028dbc95941d6f8c0aa
ziyi2 <18768107826@163.com>
2022-10-08 18:55:29 +0800

    feat: browser es module test

--- app.js
+++ app.js
@@ -2,6 +2,5 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/node_modules'));
 console.log('server start at http://localhost:3000');
 app.listen(3000);
